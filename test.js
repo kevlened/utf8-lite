172a3c40fb0ff09de3bcf503401ef81aecdb1f83
kevlened <boyettel@gmail.com>
2017-12-31 14:26:23 -0500

    1.0.0

--- test.js
+++ test.js
@@ -6,4 +6,3 @@ assert.equal(utf8.encode('hello 你好'), 'hello \xE4\xBD\xA0\xE5\xA5\xBD')
 assert.equal(utf8.decode('hello \xE4\xBD\xA0\xE5\xA5\xBD'), 'hello 你好')
 
 console.log('✅ Success!')
-process.exit(0)
